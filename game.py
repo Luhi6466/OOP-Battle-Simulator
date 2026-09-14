74460bb1b13da583bfe9d78e7c12cc4878d18c57
Luhi6466 <jbland6466@gmail.com>
2026-09-14 13:02:31 -0500

    Added hero class and actions

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "room 212"
 
@@ -12,11 +12,20 @@ def main():
 
     goblin = Goblin("Gribble")
     gooblin = Goblin("Nibble")
+    hero = Hero("Justin")
     
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print(f"{gooblin.name} enters the arena with {goblin.health} health.")
-    print("But no hero has answered the call... yet.")
+    print(f"{hero.name}  enters the arena with {hero.health} health.")
+    print(hero.battle_cry)
 
-
+    print(f"{hero.name} attacks {gooblin.name}.")
+    attack = hero.attack()
+    gooblin.take_damage(attack)
+    print(f"{gooblin.name} fights back.")
+    g2attack = gooblin.attack()
+    hero.take_damage(g2attack)
+    
+    
 if __name__ == "__main__":
     main()
--- hero.py
+++ hero.py
@@ -1,4 +1,22 @@
-class Hero:
-    """The hero blueprint will be implemented later in the project."""
+import random
 
-    pass
+class Hero:
+    """A playable character who battles enemies in the arena"""
+
+    def __init__(self, name):
+        self.name = name
+        self.health = 125
+        self.attack_power = 20
+
+    def attack(self):
+        return random.randint(1, self.attack_power)
+
+    def battle_cry():
+        print("I'll gut ya like a fish")
+
+    def take_damage(self, damage):
+        self.health = max(0, self.health - damage)
+        print(f"{self.name} takes {damage} damage. Health: {self.health}")
+
+    def is_alive(self):
+            return self.health > 0
